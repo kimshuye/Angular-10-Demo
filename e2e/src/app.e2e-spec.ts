be552ed18b7c40b0456e8ed22c649d55531a2467
Suntisuk Khemphed <suntisuk.go@gmail.com>
2021-01-05 18:51:09 +0700

    [Modified]: Project name is angular10demo

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('angular8demo app is running!');
+    expect(page.getTitleText()).toEqual('angular10demo app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular8demo'`, () => {
+  it(`should have as title 'angular10demo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular8demo');
+    expect(app.title).toEqual('angular10demo');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular8demo app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular10demo app is running!');
   });
 });
